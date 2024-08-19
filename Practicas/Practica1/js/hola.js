f0ec33728a58dc626077e20b4bc71fad7c2e2a06
JAlcon00 <c.jesusalmanza@gmail.com>
2024-08-19 09:11:30 -0600

    Practica 1

---
+++ Practicas/Practica1/js/hola.js
@@ -0,0 +1,5 @@
+var mensaje = 'Hola Mundo';
+console.log(mensaje);
+var numero = 10;
+numero = 10.50;
+console.log(numero);
---
+++ Practicas/Practica1/src/hola.ts
@@ -0,0 +1,7 @@
+let mensaje:string = 'Hola Mundo';
+
+console.log(mensaje);
+
+let numero:number = 10;
+numero = 10.50;
+console.log(numero);
