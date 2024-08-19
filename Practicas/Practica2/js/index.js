a7b450f59572baa16e2ff752cc78029eb2329c44
JAlcon00 <c.jesusalmanza@gmail.com>
2024-08-19 09:35:48 -0600

    Practica 2

---
+++ Practicas/Practica2/js/index.js
@@ -0,0 +1,9 @@
+var miNumero = 10;
+miNumero = 10 + 10;
+miNumero = miNumero + 10;
+console.log(miNumero); // 30
+var booleano = true;
+booleano = false;
+booleano = !booleano;
+var encendido = true;
+console.log(encendido); // true
---
+++ Practicas/Practica2/src/index.ts
@@ -0,0 +1,13 @@
+let miNumero:number = 10;
+
+miNumero = 10+10;
+miNumero = miNumero + 10;
+
+console.log(miNumero); // 30
+
+let booleano : boolean = true;
+booleano = false;
+booleano = !booleano;
+
+let encendido : boolean = true;
+console.log(encendido); // true
