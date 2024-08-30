b7f0152e7c84bf948ab54a16d783c6cddf625e53
JAlcon00 <c.jesusalmanza@gmail.com>
2024-08-29 20:29:10 -0600

    Cambios del reto solicitado, resultaron satisfactorios en la practica 3

--- Practicas/Practica3/js/index.js
+++ Practicas/Practica3/js/index.js
@@ -2,11 +2,11 @@ var normalx = 'Hola mundo';
 normalx = 10;
 normalx = true;
 console.log(normalx);
-var usuario = 'Hola nuevo mundo ';
+var usuario = 'Hola nuevo mundo';
 console.log(usuario);
 var value;
 value = true;
-value = 'hola undo';
+value = 'hola mundo';
 value = [];
 value = {};
 value = Math.random;
@@ -14,7 +14,7 @@ value = undefined;
 value = null;
 console.log(value);
 var miValor;
-console.log("tipo unkwon:", miValor);
+console.log("tipo unknown:", miValor);
 var value1 = miValor;
 var value2 = miValor;
 //let value3: boolean = miValor; esto marca un error
@@ -22,15 +22,18 @@ var value2 = miValor;
 var edad = 20;
 console.log('edad:', edad);
 var sueldo = 1.201;
-var total = Number('14785'); //castear un string a number
+var total = Number('14785'); // castear un string a number
 console.log('Total:', total);
 var personaje = "El chavo del 8";
 var otroPersonaje = 'El chapulin colorado';
+/**
+ * Represents another character in the story.
+ */
 var otroPersonajeMas = "El chompiras y ".concat(personaje);
 console.log('Personaje:', personaje);
 console.log('Otro personaje:', otroPersonaje);
 var heroes = ['Batman', 'Superman', 'Robin'];
-var heroe = "Deadpools House";
+var heroe = "Deadpool's House";
 console.log(heroe);
 console.log(heroe.toUpperCase());
 var vengador;
@@ -40,3 +43,58 @@ vengador = 150.55;
 console.log(vengador.toFixed(1));
 console.log(vengador.toFixed(1));
 console.log(vengador.toFixed(1));
+var personas = ['Peter', 'Tony', 'Steve'];
+console.log(personas);
+console.log(personas[0].charAt(0));
+console.log(personas[1].charAt(0));
+console.log(personas[2].charAt(0));
+personas.push('Thor');
+console.log(personas);
+var numeros = [1, 2, 3, 4, 5];
+var numeros2 = [6, 7, 8, 9, 10];
+console.log(numeros, numeros2);
+function imprimirNumeros() {
+    var numeros = [1, 2, 3, 4, 5];
+    var numeros2 = [6, 7, 8, 9, 10];
+    console.log(numeros, numeros2);
+    // Print the numbers in the HTML
+    var numerosElement = document.getElementById('numeros');
+    var numeros2Element = document.getElementById('numeros2');
+    if (numerosElement && numeros2Element) {
+        numerosElement.innerHTML = numeros.join('');
+        numeros2Element.innerHTML = numeros2.join('');
+    }
+}
+imprimirNumeros();
+var miArray = ['Hola', 10, 'Mundo', 20];
+console.log(miArray);
+var miArray2 = ['Mundo', 20, 'Hola', 30];
+console.log(miArray2);
+var miArray3 = [true, 'Hola', 10, 'Mundo', 20];
+console.log(miArray3);
+var miArray4 = [1, 2, 3, 4, 5];
+function imprimirAlReves(arr) {
+    var reversedArray = arr.reverse();
+    var miArray4Element = document.getElementById('miArray4');
+    if (miArray4Element) {
+        miArray4Element.innerHTML = reversedArray.join(', ');
+    }
+}
+function actualizarHTML() {
+    var miArrayElement = document.getElementById('miArray');
+    var miArray2Element = document.getElementById('miArray2');
+    var miArray3Element = document.getElementById('miArray3');
+    if (miArrayElement) {
+        miArrayElement.innerHTML = miArray.join(', ');
+    }
+    if (miArray2Element) {
+        miArray2Element.innerHTML = miArray2.join(', ');
+    }
+    if (miArray3Element) {
+        miArray3Element.innerHTML = miArray3.join(', ');
+    }
+}
+document.addEventListener('DOMContentLoaded', function (event) {
+    actualizarHTML();
+    imprimirAlReves(miArray4);
+});
--- Practicas/Practica3/src/index.ts
+++ Practicas/Practica3/src/index.ts
@@ -1,16 +1,16 @@
-let normalx:any = 'Hola mundo';
+let normalx: any = 'Hola mundo';
 normalx = 10;
 normalx = true;
 
 console.log(normalx);
 
-let usuario: string = 'Hola nuevo mundo '
-console.log(usuario)
+let usuario: string = 'Hola nuevo mundo';
+console.log(usuario);
 
 let value: any;
 
 value = true;
-value = 'hola undo';
+value = 'hola mundo';
 value = [];
 value = {};
 value = Math.random;
@@ -20,7 +20,7 @@ value = null;
 console.log(value);
 
 let miValor: unknown;
-console.log("tipo unkwon:",miValor);
+console.log("tipo unknown:", miValor);
 
 let value1: unknown = miValor;
 let value2: any = miValor;
@@ -32,26 +32,94 @@ const edad: number = 20;
 console.log('edad:', edad);
 
 const sueldo: number = 1.201;
-const total: number = Number('14785');//castear un string a number
+const total: number = Number('14785'); // castear un string a number
 console.log('Total:', total);
 
-
-const personaje:string =  `El chavo del 8`;
+const personaje: string = `El chavo del 8`;
 const otroPersonaje: string = 'El chapulin colorado';
+/**
+ * Represents another character in the story.
+ */
 const otroPersonajeMas: string = `El chompiras y ${personaje}`;
 console.log('Personaje:', personaje);
 console.log('Otro personaje:', otroPersonaje);
 
 const heroes: string[] = ['Batman', 'Superman', 'Robin'];
-const heroe:string =  `Deadpools House`;
+const heroe: string = `Deadpool's House`;
 
 console.log(heroe);
 console.log(heroe.toUpperCase());
 
-let vengador:any;
+let vengador: any;
 vengador = 'Dr. Strange';
 console.log(vengador.charAt(4));
 vengador = 150.55;
 console.log(vengador.toFixed(1));
 console.log((vengador as number).toFixed(1));
 console.log(<number>vengador.toFixed(1));
+
+let personas: string[] = ['Peter', 'Tony', 'Steve'];
+console.log(personas);
+console.log(personas[0].charAt(0));
+console.log(personas[1].charAt(0));
+console.log(personas[2].charAt(0));
+personas.push('Thor');
+console.log(personas);
+
+let numeros: number[] = [1, 2, 3, 4, 5];
+let numeros2: Array<number> = [6, 7, 8, 9, 10];
+console.log(numeros, numeros2);
+
+function imprimirNumeros() {
+    let numeros: number[] = [1, 2, 3, 4, 5];
+    let numeros2: Array<number> = [6, 7, 8, 9, 10];
+    console.log(numeros, numeros2);
+
+    // Print the numbers in the HTML
+    const numerosElement = document.getElementById('numeros');
+    const numeros2Element = document.getElementById('numeros2');
+
+    if (numerosElement && numeros2Element) {
+        numerosElement.innerHTML = numeros.join('');
+        numeros2Element.innerHTML = numeros2.join('');
+    }
+}
+
+imprimirNumeros();
+
+let miArray: (string | number)[] = ['Hola', 10, 'Mundo', 20];
+console.log(miArray);
+let miArray2: Array<string | number> = ['Mundo', 20, 'Hola', 30];
+console.log(miArray2);
+let miArray3: Array<any> = [true, 'Hola', 10, 'Mundo', 20];
+console.log(miArray3);
+let miArray4: number[] = [1, 2, 3, 4, 5];
+
+function imprimirAlReves(arr: number[]): void {
+    const reversedArray = arr.reverse();
+    const miArray4Element = document.getElementById('miArray4');
+    if (miArray4Element) {
+        miArray4Element.innerHTML = reversedArray.join(', ');
+    }
+}
+
+function actualizarHTML() {
+    const miArrayElement = document.getElementById('miArray');
+    const miArray2Element = document.getElementById('miArray2');
+    const miArray3Element = document.getElementById('miArray3');
+
+    if (miArrayElement) {
+        miArrayElement.innerHTML = miArray.join(', ');
+    }
+    if (miArray2Element) {
+        miArray2Element.innerHTML = miArray2.join(', ');
+    }
+    if (miArray3Element) {
+        miArray3Element.innerHTML = miArray3.join(', ');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    actualizarHTML();
+    imprimirAlReves(miArray4);
+});
