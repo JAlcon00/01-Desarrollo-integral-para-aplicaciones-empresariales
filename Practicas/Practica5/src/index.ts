0f2477268e6b69c35abb92db2fc88c2c4be2e0ff
JAlcon00 <c.jesusalmanza@gmail.com>
2024-09-06 18:32:37 -0600

    Practica 4 y 5

---
+++ Practicas/Practica5/src/index.ts
@@ -0,0 +1,16 @@
+let miVariable: string = 'hola mundo';
+
+console.log(miVariable);
+
+const person1 = {
+    firstName1: "Jesus",
+    lastName1: "Almanza",
+    age: 25,
+
+}
+
+let {firstName1, lastName1} = person1;
+console.log(firstName1,'', lastName1);
+
+let miNombre = "Yisus";
+let [a1, a2, a3, a4, a5] 
