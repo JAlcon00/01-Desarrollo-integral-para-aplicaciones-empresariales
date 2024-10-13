c3ef6fce8fdbc08e0c2b48fd3ea32da7072117dc
JAlcon00 <c.jesusalmanza@gmail.com>
2024-10-13 08:32:17 -0600

    Practica 17 (Materia practica 16)

---
+++ Practicas/Practica17/index.ts
@@ -0,0 +1,52 @@
+import * as http from 'http';
+import * as fs from 'fs';
+const PUERTO = 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.setHeader('Content-Type', 'text/html');
+
+    // Simple Router
+    let ruta = './views';
+    switch (req.url) {
+        case '/':
+            ruta += '/index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            ruta += '/about.html';
+            res.statusCode = 200;
+            break;
+        case '/productos':
+            ruta += '/productos.html';
+            res.statusCode = 200;
+            break;
+        case '/servicios':
+            ruta += '/servicios.html';
+            res.statusCode = 200;
+            break;
+        case '/contact':
+            ruta += '/contacto.html';
+            res.statusCode = 200;
+            break;
+        default:
+            ruta += '/404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(ruta, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+
+});
+
+server.listen(PUERTO, () => {
+    console.log('Servidor en el puerto 3000');
+});
